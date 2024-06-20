46a1534c04e16fa81d72346dfdb6bd6fd24d4c14
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-06-20 15:28:55 +0200

    fetched images from sourse

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/\hero_banner.jpg'
 
 const Home = () => {
   return (
     <div className='home'>
     <Navbar/>
+    <div className='hero'>
+      <img src={hero_banner} alt="" className='hero_banner'/>
+    </div>
     </div>
   )
 }
