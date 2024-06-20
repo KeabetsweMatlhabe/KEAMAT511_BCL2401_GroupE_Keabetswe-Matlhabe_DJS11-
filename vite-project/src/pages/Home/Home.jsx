78b437ae489be0f6872084899597334dc29d5aca
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-06-20 16:53:16 +0200

    implemented the button code

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-import hero_banner from '../../assets/\hero_banner.jpg'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div className='home'>
-    <Navbar/>
-    <div className='hero'>
-      <img src={hero_banner} alt="" className='hero_banner'/>
-    </div>
+     <Navbar/>
+     <div className='hero'>
+      <img src={hero_banner} alt="" className='banner-img'/>
+      <div className="hero-caption">
+        <img src={hero_title} alt="" className='caption-img'/>
+      <p>Dicovering his ties to a secret ancient order,a young man living in modern Istanbul emburks on a quest to save the city from</p>
+      <div className="hero-btns"></div>
+      <button className='btn'><img src={play_icon} alt=""/>Play</button>
+      <button className='btn dark-btn'><img src={info_icon_icon} alt=""/>More Info</button>
+      </div>
+     </div>
     </div>
   )
 }
