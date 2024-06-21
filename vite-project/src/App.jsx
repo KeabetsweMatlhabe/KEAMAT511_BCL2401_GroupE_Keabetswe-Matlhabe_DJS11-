40a75181398f6240a3557cfff6aaf04372843d9e
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-06-21 11:09:13 +0200

    made exports on the series page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,92 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Layout from "./pages/Layout";
-import Podcasts from "./pages/Podcasts";
-import Favorites from "./pages/Favorites";
-import PodcastDetails from "./pages/PodcastDetails";
+import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// routes
+import {
+  Home,
+  Signup,
+  SinglePodcast,
+  Login,
+  Favourites,
+  PageNotFound,
+} from './components/Header';
+import supabase from './supabase/client';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setAllPodcasts,
+  setIsLoading,
+  setHomePageDisplayedPodcasts,
+  setUserDataFromDB,
+  setHasAccount,
+  setFavourites,
+  setSortSearchFavouritesArray,
+} from './globalState/reducers/podcastsReducer';
 
-export const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="podcasts" element={<Podcasts />} />
-          <Route path="podcasts/:id" element={<PodcastDetails />} />
-
-          <Route path="favorites" element={<Favorites />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+function App() {
+  const { favourites, favouriteSwitch } = useSelector(
+    (state) => state.podcastsReducer
   );
-};
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setIsLoading(true));
+    const getPodcasts = async () => {
+      const response = await fetch('https://podcast-api.netlify.app/shows');
+      const result = await response.json();
+
+      if (result) {
+        dispatch(setHomePageDisplayedPodcasts(result));
+        dispatch(setAllPodcasts(result));
+
+        dispatch(setIsLoading(false));
+      } else {
+        console.log('error');
+      }
+      return result;
+    };
+    getPodcasts();
+  }, []);
+
+  const fetchLoginData = async () => {
+    const { data, error } = await supabase.from('user_login_data').select();
+    if (error) {
+      console.log(error);
+    }
+    if (data.length !== 0) {
+      dispatch(setUserDataFromDB(data));
+      dispatch(setHasAccount(true));
+    }
+  };
+  useEffect(() => {
+    fetchLoginData();
+  }, []);
+
+  const fetchFavouritesFromDB = async () => {
+    const { data, error } = await supabase.from('userFavourites').select();
+    if (data) {
+      dispatch(setFavourites(data));
+    }
+  };
+  useEffect(() => {
+    fetchFavouritesFromDB();
+  }, [favouriteSwitch]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/podcast/:id" element={<SinglePodcast />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
 export default App;
--- vite-project/src/components/Layout.jsx
+++ vite-project/src/components/Layout.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = () => {
+export default function Layout() {
   return (
     <div className="site-wrapper">
-      <Navbar />
+      <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
-};
-
-export default Layout;
+}
